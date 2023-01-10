42e352b7f0aff4ecb44fee0e2f206c4cb314d0a2
y-nekonuko <hyuki222@gmail.com>
2023-01-10 23:50:49 +0900

    計算アプリ実装

--- src/components/pages/ChatGptApp.jsx
+++ src/components/pages/ChatGptApp.jsx
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
 
-import { InputToDo, Filter, ToDo } from "../organisms/index";
-
 export const ChatGptApp = () => {
-  return <div className="container"></div>;
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    const value = e.target.innerText;
+    if (value === "c") {
+      setResult("");
+    } else if (value === "=") {
+      try {
+        setResult(eval(result).toString());
+      } catch (e) {
+        setResult("error");
+      }
+    } else {
+      setResult(result + value);
+    }
+  };
+
+  return (
+    <div className="container">
+      <input type="text" id="result" value={result} />
+      <br />
+      <button onClick={handleClick}>1</button>
+      <button onClick={handleClick}>2</button>
+      <button onClick={handleClick}>3</button>
+      <button onClick={handleClick}>+</button>
+      <br />
+      <button onClick={handleClick}>4</button>
+      <button onClick={handleClick}>5</button>
+      <button onClick={handleClick}>6</button>
+      <button onClick={handleClick}>-</button>
+      <br />
+      <button onClick={handleClick}>7</button>
+      <button onClick={handleClick}>8</button>
+      <button onClick={handleClick}>9</button>
+      <button onClick={handleClick}>*</button>
+      <br />
+      <button onClick={handleClick}>c</button>
+      <button onClick={handleClick}>0</button>
+      <button onClick={handleClick}>=</button>
+      <button onClick={handleClick}>/</button>
+    </div>
+  );
 };
 
 export default ChatGptApp;
